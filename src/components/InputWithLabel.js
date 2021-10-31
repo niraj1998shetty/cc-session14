13a0a78d4a21bfb44c3d6e390b21adfefdd24329
niraj1998shetty <87310954+niraj1998shetty@users.noreply.github.com>
2021-10-31 12:52:29 +0530

    Delete src/components directory

--- src/components/InputWithLabel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InputWithLabel=({value,id,children,type,onInputChange})=>{
-    return <>
-      <label htmlFor={id}>{children} </label>
-            <input type={type} id={id} value={value} onChange={onInputChange}></input>
-    </>
-}
-export default InputWithLabel;
--- src/components/Search.js
+++
@@ -1,9 +0,0 @@
-const Search=({searchTerm,onChange})=>{
-    return(
-        <>
-            <label>Search for stories: </label>
-            <input type="text" value={searchTerm}onChange={onChange}></input>
-        </>
-    )
-}
-export default Search
